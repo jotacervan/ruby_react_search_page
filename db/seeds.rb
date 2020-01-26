f1aba05125d3dfc61662a6212e2dec0a002988c8
jotacervanumanni <jota@umanni.com.br>
2020-01-26 16:12:15 -0300

    Create seed to populate database

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,11 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+path = File.join(File.dirname(__FILE__), "./seed_files/SpocketProducts.json")
+records = JSON.parse(File.read(path))
+records.each do |record|
+  country = Country.find_or_create_by(name: record['country']) 
+  product = Product.create(name: record['title'], description: record['description'], price: record['price'], country: country)
+  tags = record['tags'].split(',').map do |t|
+    tag = Tag.find_or_create_by(name: t.strip)
+    product.product_tags.create(tag: tag)
+    tag
+  end
+end
